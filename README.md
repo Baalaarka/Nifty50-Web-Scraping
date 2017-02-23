bc6b4234fc97062ecd65efbeb12baa751c70ba4d
Balakrishna Kothapalli <Balakrishna Kothapalli>
2017-02-24 03:35:35 +0530

    gitignore added and timedelta changes in celery beat

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Nifty50-Web-Scraping
 
 1. pip install -r requirements.txt
-2. cd Zero and run python app.py
-3. open localhost:8080
+2. cd Zero
+3. celery -A tasks worker --loglevel=info (background periodic worker for scraping)
+4. run python app.py in a new terminal session
+5. open localhost:8080
--- tasks.py
+++ tasks.py
@@ -1,6 +1,7 @@
 from celery import Celery
-from celery.schedules import crontab
+# from celery.schedules import crontab
 from scrap import scrap_data
+from datetime import timedelta
 
 
 app = Celery('tasks', broker='redis://139.59.31.78')
@@ -8,8 +9,8 @@ app = Celery('tasks', broker='redis://139.59.31.78')
 app.conf.beat_schedule = {
     # Executes every Monday morning at 7:30 a.m.
     'scheduled_scraping_task': {
-        'task': 'tasks.scrapy_nifty',
-        'schedule': crontab(minute='*/1'),
+        'task': 'tasks.scrap_nifty',
+        'schedule': timedelta(seconds=300),
         'args': (),
     },
 }
